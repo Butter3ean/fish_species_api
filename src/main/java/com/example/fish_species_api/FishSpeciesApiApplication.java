834df43ebf78d0107daff6aaac0ff85510211405
Butter3ean <dhughes3223@gmail.com>
2023-04-02 15:46:30 -0400

    Initial commit

---
+++ src/main/java/com/example/fish_species_api/FishSpeciesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fish_species_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishSpeciesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishSpeciesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fish_species_api/FishSpeciesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fish_species_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishSpeciesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
